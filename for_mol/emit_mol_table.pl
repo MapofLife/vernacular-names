18e2c19943ca5b2df89355db1cab9928fcd318fd
gaurav <gaurav@ggvaidya.com>
2014-06-18 22:33:06 -0600

    Updated source to include nwpalms (updated).

--- for_mol/emit_mol_table.pl
+++ for_mol/emit_mol_table.pl
@@ -71,7 +71,7 @@ my $db = DBI->connect("dbi:Pg:dbname=common_names;host=127.0.0.1;port=5432",
 # Retrieve a list of all species containing names in Latin, which should
 # only be the index of names which exist in Map of Life.
 my $languages = join(', ', map { "'$_'" } @LANGUAGES);
-my $sth = $db->prepare("SELECT binomial FROM entries WHERE LOWER(lang) = 'la' AND (source = 'EOL API calls week of February 9 to 15, 2014' OR source = 'EOL API calls, March 29 to 30, 2014' OR source = 'EOL API calls, May 2, 2014: new_world_palms' OR source = 'EOL API calls, May 2, 2014: na_trees') GROUP BY binomial");
+my $sth = $db->prepare("SELECT binomial FROM entries WHERE LOWER(lang) = 'la' AND (source = 'EOL API calls week of February 9 to 15, 2014' OR source = 'EOL API calls, March 29 to 30, 2014' OR source = 'EOL API calls, May 2, 2014: new_world_palms (updated 2014-June-18)' OR source = 'EOL API calls, May 2, 2014: na_trees') GROUP BY binomial");
 $sth->execute;
 my $scientific_names = $sth->fetchall_arrayref([0]);
 
