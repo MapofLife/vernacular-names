33148ab52894726005e45bf9caaec422d0980f46
gaurav <gaurav@ggvaidya.com>
2015-06-21 18:47:24 -0600

    Added support for Appstats.

--- app/appengine_config.py
+++ app/appengine_config.py
@@ -5,3 +5,9 @@ import os.path
 sys.path.insert(0, os.path.join(os.path.dirname(__file__), 'config'))
 sys.path.insert(0, os.path.join(os.path.dirname(__file__), 'lib/python-titlecase'))
 sys.path.insert(0, os.path.join(os.path.dirname(__file__), 'lib/urlfetch'))
+
+# Activate Appstats (from https://cloud.google.com/appengine/docs/python/tools/appstats)
+def webapp_add_wsgi_middleware(app):
+  from google.appengine.ext.appstats import recording
+  app = recording.appstats_wsgi_middleware(app)
+  return app
